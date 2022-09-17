2ec83e225a94d4fde9e42c6ded7dd202c7f8ee9c
volumio <albasoft2020@gmail.com>
2022-09-17 12:50:34 +0000

    Started implementing routines for scrobble cache

--- lastfm/lib/lastfmPromises.js
+++ lastfm/lib/lastfmPromises.js
@@ -8,6 +8,16 @@ var defaults = require('./defaults');
 var LastFmNode = require('lastfm').LastFmNode;
 var libQ = require('kew');
 
+// Added persistent caching of scrobbles
+var cache = require('persistent-cache');
+const retryOnErrors = [
+      11,                 // Service offline
+      16,                 // Temporarily unavailable
+      29                  // Rate limit exceeded
+    ];
+var scrobbleCache;
+
+
 var LastfmAPI = module.exports = function (options) {
 	this.api = new LastFmNode(options);
 	this.sessionCredentials = null;
@@ -45,6 +55,7 @@ LastfmAPI.prototype.updateNowPlaying = function (track) {
 LastfmAPI.prototype.getTrackInfo = function (track) {
 	var defer = libQ.defer();
     var options = defaults.promiseOptions(track, defer, 'track');
+    if (this.sessionCredentials && this.sessionCredentials.username) options.username = this.sessionCredentials.username;
 	this.api.request('track.getInfo', options);
     return defer.promise;
 };
@@ -69,6 +80,7 @@ LastfmAPI.prototype.scrobble = function (tracks) {
 		tracks = newParams;
 	}
     var defer = libQ.defer();
+//    var defer = new Promise();
     var options = defaults.promiseOptions(tracks, defer, 'scrobbles');
 	options.sk = this.sessionCredentials.key;
 	this.api.request('track.scrobble', options);
@@ -78,6 +90,7 @@ LastfmAPI.prototype.scrobble = function (tracks) {
 LastfmAPI.prototype.getArtistInfo = function (artist) {
 	var defer = libQ.defer();
     var options = defaults.promiseOptions(artist, defer, 'artist');
+    if (this.sessionCredentials && this.sessionCredentials.username) options.username = this.sessionCredentials.username;
 	this.api.request('artist.getInfo', options);
     return defer.promise;
 };
@@ -89,3 +102,65 @@ LastfmAPI.prototype.getSimilarArtists = function (artist) {
 	this.lastfm.api.request('artist.getSimilar', options);
     return defer.promise;
 };
+
+// Routines for using a persistent cache----------------------------------------------------------------------------------
+// e.g. options {base:'.', name:'scrobbles'}
+LastfmAPI.prototype.initCache = function (options) {
+    options = options || { name:'scrobbles' };
+    scrobbleCache = cache(options);
+};
+
+LastfmAPI.prototype.scrobbleToCache = function (track) {
+    return new Promise((resolve, reject) => {
+        if (track && track.artist && track.track) { // enough data for scrobbling
+            track.timestamp = track.timestamp || Math.floor(Date.now() / 1000);  // if timestamp is missing use 'now'
+            // add to cache using timestamp as the key
+            scrobbleCache.put(track.timestamp, track, function(err) {
+                //check err for errors
+                if (err) reject('Failed to add track to cache');
+                else resolve('Added track to cache');
+            });
+        } 
+        else reject('Not enough data in track to be a valid scrobble.');
+    });
+};
+
+LastfmAPI.prototype.scrobbleCachedData = function (options) {
+    let self = this;
+    if (!scrobbleCache) self.initCache(options);  // Make sure cache is set up!
+    return new Promise((resolve, reject) => {
+        scrobbleCache.keys(function(err, keys) {
+            //Handle errors
+            if (err) return reject(err);
+            
+            console.log("Scrobble cache keys: ", keys); 
+            if (keys.length > 0) {
+                keys.forEach(key => {
+                    console.log(JSON.stringify(scrobbleCache.getSync(key)));
+                    self.scrobble(scrobbleCache.getSync(key))
+                        .then(resp => {
+                            scrobbleCache.deleteSync(key);
+            //                console.log('Appected: ' + resp['@attr']['accepted']);
+                            if (resp['@attr'].accepted > 0){
+//                                console.log(resp.scrobble.artist['#text']+ ' - ' + (resp.scrobble.track['#text'] || 'unknown'));// + ' (' +(resp.scrobble.album['#text'] || 'unknown' ));
+                                resolve(resp.scrobble.artist['#text']+ ' - ' + (resp.scrobble.track['#text'] || 'unknown'));
+                            }
+                            else {
+//                                console.log('Failed ');
+                                reject('Failed');
+                            }
+                        })
+                        .fail(err => {
+                            // If it is a lastFM error then err.error should be defined and a number code. If not there has been some other error, such as network connection down.
+                             if (!err.error || retryOnErrors.includes(err.error)) console.log('Failed to scrobble; keep in cache');
+                             else  {
+//                                console.log('Failed to scrobble. Will remove track from cache');
+                                reject('Failed to scrobble. Will remove track from cache');
+                                scrobbleCache.deleteSync(key);
+                            } 
+                        });  
+                });
+            } else resolve('Cache was empty. No tracks scrobbled.')
+        });
+    });
+};    
